fdaf3621042355e2baf616f8cd039727f56f4cf9
Luca Maraschi <paomaras@Lucas-MacBook-Pro.local>
2016-10-31 23:48:30 +0100

    my .files

--- install/osx/initial-osx.sh
+++ install/osx/initial-osx.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+echo "Checking Xcode CLI tools"
+# Only run if the tools are not installed yet
+# To check that try to print the SDK path
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Xcode CLI tools not found. Installing them..."
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$PROD" -v;
+else
+  echo "Xcode CLI tools OK"
+fi
+
 echo "Cloning Dracula iTerm theme"
 git clone https://github.com/dracula/iterm.git ~/iterm/themes
 echo -e "\nDracula theme cloned, import the theme in iTerm\n"
