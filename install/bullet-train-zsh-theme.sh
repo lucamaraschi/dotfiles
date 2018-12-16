c1304aafdd734eb8e1e7d10ac7d30fce75ca3854
Luca Maraschi <luca.maraschi@gmail.com>
2018-12-15 19:28:08 -0800

    ZSH

--- install/bullet-train-zsh-theme.sh
+++ install/bullet-train-zsh-theme.sh
@@ -3,4 +3,4 @@
 echo "\nInstalling bullet-train ZSH theme\n"
 
 cd ~/.oh-my-zsh/themes
-wget http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
+wget https://raw.githubusercontent.com/caiogondim/bullet-train.zsh/master/bullet-train.zsh-theme
--- install/spaceship-prompt.sh
+++ install/spaceship-prompt.sh
@@ -3,6 +3,8 @@
 echo "\nInstalling spaceship-prompt ZSH theme\n"
 echo "\nCloning repo & Creating a symlink to zsh themes folder\n"
 
+export ZSH_CUSTOM=~/.oh-my-zsh/themes
+
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
