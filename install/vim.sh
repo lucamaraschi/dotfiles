05c66592be17fe3fc74c0f0c50511ffae0daa707
Luca Maraschi <luca.maraschi@gmail.com>
2020-02-14 22:18:08 -0800

    Update vim.sh

--- install/vim.sh
+++ install/vim.sh
@@ -4,5 +4,7 @@ echo '===========> Configuring VIM'
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+vim +PluginInstall +qall
+
 # Enable dracula so we can install the Plugins without errors ;-)
 sed -i "" 's/"color dracula/color dracula/g' > /dev/null
